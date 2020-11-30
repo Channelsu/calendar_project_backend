a7b6a4c84c0b5c62ec2292184048528179148652
Channelsu <h_sugimoto0401@yahoo.co.jp>
2020-11-30 22:50:32 +0900

    フロントエンドと通信テスト処理追記

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,14 @@ router.get('/test', function(req, res, next) {
   });
 });
 
+router.post('/testpost', function(req, res, next) {
+  console.log('req.body→', req.body);
+  let test = {name: 'テストです', date: '20201130'}
+  return res.status(200).json({
+    success: true,
+    message: '成功です！',
+    object: test,
+  });
+});
+
 module.exports = router;
