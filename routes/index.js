ea0db61d4cc851f7be3c163ce3a674edb082974f
Channelsu <h_sugimoto0401@yahoo.co.jp>
2020-11-30 21:38:44 +0900

    テストルートの作成

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let test = {name: 'ルートです', date: '20201130'}
+  return res.status(200).json({
+    success: true,
+    message: '成功です！',
+    object: test,
+  });
+});
+
+router.get('/test', function(req, res, next) {
+  let test = {name: 'テストです', date: '20201130'}
+  return res.status(200).json({
+    success: true,
+    message: '成功です！',
+    object: test,
+  });
 });
 
 module.exports = router;
