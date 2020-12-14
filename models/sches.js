4a7e642be4f4c199bcdf355cf1e59ece0c58ae53
Channelsu <h_sugimoto0401@yahoo.co.jp>
2020-12-14 21:56:49 +0900

    user_idカラム削除

--- models/sches.js
+++ models/sches.js
@@ -14,7 +14,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   sches.init({
-    user_id: DataTypes.INTEGER,
     title: DataTypes.STRING,
     start_date: DataTypes.STRING,
     start_time: DataTypes.STRING,
