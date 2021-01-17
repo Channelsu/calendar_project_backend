628acb06f166a9b95c22c4776731d6a5bd2b113a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-01-17 17:08:07 +0900

    ログインテスト用のルーティング作成

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,24 @@ router.get('/test', function(req, res, next) {
   });
 });
 
+router.post('/login', function(req, res, next) {
+  console.log('login➡️', req.body);
+  let test = {name: 'テストです', date: '20201130'};
+  return res.status(200).json({
+    success: true,
+    message: '成功です！',
+    object: test,
+  });
+});
+
+router.get('/login/me', function(req, res, next) {
+  console.log('login/me➡️', req.headers);
+  let test = {name: 'テストです', date: '20201130'};
+  return res.status(200).json({
+    success: true,
+    message: '成功です！',
+    object: test,
+  });
+});
+
 module.exports = router;
